0c8dcee04341c6c3c58ca32798673122c696c7db
Joachim Brendborg <201807077@post.au.dk>
2024-06-16 20:41:45 +0200

    Implemented smoothing

--- display.py
+++ display.py
@@ -1,7 +1,7 @@
 from mandelbrot_set import MandelbrotSet
 from PIL import Image
 
-mandelbot_set = MandelbrotSet(max_iterations=20)
+mandelbrot_set = MandelbrotSet(max_iterations=20, escape_radius=2)
 
 width, height = 512, 512
 scale = 0.0075
@@ -12,7 +12,9 @@ image = Image.new(mode=GREYSCALE, size=(width, height))
 for i in range(height):
     for j in range(width):
         c = scale * complex(j - width / 2, height / 2 - i)
-        instability = 1 - mandelbot_set.stability(c)
+        instability = 1 - mandelbrot_set.stability(c, smooth=False)
         image.putpixel((j,i), int(255 * instability))
+
+
 
 image.show()
--- mandelbrot_set.py
+++ mandelbrot_set.py
@@ -1,20 +1,23 @@
 from dataclasses import dataclass
+from math import log
 
 @dataclass
 class MandelbrotSet:
     max_iterations: int 
-
+    escape_radius: float = 2.0
 
     def __contains__(self, c: complex) -> bool:
         return self.stability(c) == 1
 
-    def stability(self, c: complex) -> float:
-        return self.escape_count(c) / self.max_iterations
+    def stability(self, c: complex, smooth=False) -> float:
+        return self.escape_count(c, smooth) / self.max_iterations
     
-    def escape_count(self, c: complex) -> int:
+    def escape_count(self, c: complex, smooth=False) -> int | float:
         z = 0
-        for iterations in range(self.max_iterations):
+        for iteration in range(self.max_iterations):
             z = z ** 2 + c
-            if abs(z) > 2:
-                return iterations
+            if abs(z) > self.escape_radius:
+                if smooth:
+                    return iteration + 1 - log(log(abs(z))) / log(2)
+                return iteration
         return self.max_iterations
