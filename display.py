2c62a5d360fb556635078efedbf4ce970f49adbc
Joachim Brendborg <201807077@post.au.dk>
2024-06-16 18:00:10 +0200

    Greyscale image

--- display.py
+++ display.py
@@ -1,17 +1,18 @@
 from mandelbrot_set import MandelbrotSet
 from PIL import Image
 
-mandelbot_set = MandelbrotSet(max_iterations=30)
+mandelbot_set = MandelbrotSet(max_iterations=20)
 
 width, height = 512, 512
 scale = 0.0075
-BLACK_AND_WHITE = "1"
+GREYSCALE = 'L'
 
-image = Image.new(mode=BLACK_AND_WHITE, size=(width, height))
+image = Image.new(mode=GREYSCALE, size=(width, height))
 
 for i in range(height):
     for j in range(width):
         c = scale * complex(j - width / 2, height / 2 - i)
-        image.putpixel((j,i), c not in mandelbot_set)
+        instability = 1 - mandelbot_set.stability(c)
+        image.putpixel((j,i), int(255 * instability))
 
 image.show()
